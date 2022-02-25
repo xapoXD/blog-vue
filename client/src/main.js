1971362a2780ebd18692f5d7499f3c64cb2b9926
jankarasek <jan1karasek@gmail.com>
2022-02-25 16:45:52 +0100

    add new component Article.vue

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootsrap.css';
-import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 
 const app = createApp(App)
 
