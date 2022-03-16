580c758f517e59913626c3cda79b6b2540da4436
jankarasek <jan1karasek@gmail.com>
2022-03-16 18:33:00 +0100

    added better form

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,39 +1,56 @@
 <template>
-  <footer class="cs-header">
+  <footer class="cs-footer">
+
+
+
+
+
 
     <form>
+      <div class="row">
+        <div class="col-md-4 mb-3">
+          <label for="validationDefault01">First name</label>
+          <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="" required>
+        </div>
+        <div class="col-md-4 mb-3">
+          <label for="validationDefault02">Last name</label>
+          <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="" required>
+        </div>
+      </div>
+
+        <div class="col-md-4 mb-3">
+          <label for="exampleInputEmail">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email" required>
+          <small id="emailHelp1" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+          <label class="form-check-label" for="invalidCheck2">
+            Agree to terms and conditions
+          </label>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+
+      <button class="btn btn-primary" type="submit">Submit form</button>
     </form>
+
+
 
   </footer>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Footer"
 }
 </script>
 
 <style scoped>
 
-.cs-header {
+.cs-footer {
 
-
-  background-repeat: no-repeat;
-  background-position: bottom center;
   height: 40vh;
 }
 </style>
